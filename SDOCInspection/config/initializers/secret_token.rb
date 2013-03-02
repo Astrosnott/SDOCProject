5a8f156fdbdb6938b6bb5a5ed543122ce6b312cf
Austin Blake <Astrosnott@gmail.com>
2013-03-02 01:22:01 -0500

    Created a rails applications

---
+++ SDOCInspection/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SDOCInspection::Application.config.secret_token = '13b19bcdc26533d537b08d704b07c23307aa7898d9e7f5aea43ecbe0467671343b8708a1ba3405c6cb27255abeb84e7b6af4c6c5f3a220b14fa828763997cd1c'
